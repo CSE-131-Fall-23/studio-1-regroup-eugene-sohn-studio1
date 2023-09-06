4fc2d10f755bf26cb2fb437200a7ae4b6afb94b0
euges <euges@eugesoh_laptop.dhcp.wustl.edu>
2023-09-06 15:06:12 -0500

    Finished Studio 1

--- src/studio1/Ordered.java
+++ src/studio1/Ordered.java
@@ -15,7 +15,12 @@ public class Ordered {
 		int y = in.nextInt();
 		System.out.println("Value for z?");
 		int z = in.nextInt();
-
+boolean xy = (x < y);
+boolean yz = (y < z);
+boolean yx = (x > y);
+boolean zy = (y > z);
+boolean inOrder = xy && yz || yx && zy;
+System.out.println(inOrder);
 	}
 
 }
